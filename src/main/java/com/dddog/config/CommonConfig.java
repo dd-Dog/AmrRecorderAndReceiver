4b4c79e4989ea2ffa13e95877d7324413b1cc61b
bianjb <bianjb@flyscale.cn>
2018-12-05 18:49:16 +0800

    修改包名，站在巨人的肩膀上

---
+++ src/main/java/com/dddog/config/CommonConfig.java
@@ -0,0 +1,11 @@
+package com.dddog.config;
+
+//blog.csdn.net/zgyulongfei
+//Email: zgyulongfei@gmail.com
+
+public class CommonConfig {
+	public static String SERVER_IP_ADDRESS = "192.168.1.50";
+
+	public static final int AUDIO_SERVER_UP_PORT = 20086;
+	public static final int AUDIO_SERVER_DOWN_PORT = 20087;
+}
